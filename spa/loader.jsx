45e3c7e3d6b8a366e8fda1a85f32c5ebd86b7b76
alet89 <alessandrotoschi@me.com>
2020-06-17 00:16:52 +0200

    ggg

---
+++ spa/loader.jsx
@@ -0,0 +1,9 @@
+var Loader = React.createClass({
+    
+    render() { return (
+        <div class="loadingio-spinner-square-iyrjry8g9g9"><div class="ldio-wdnt6erfulc">
+        <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
+        </div></div>
+    );
+    }
+});
