d633eb47a0a123b183b08803c50d0fa8954a0de5
alet89 <alessandrotoschi@me.com>
2020-06-16 15:19:33 +0200

    donation message

--- spa/deploy/donation.jsx
+++ spa/deploy/donation.jsx
@@ -30,9 +30,9 @@ var Deploy = React.createClass({
                         <aside>&#128577; Nope!</aside>
                     </section>
                 </section>
-                <p></p>
+                <p><b>No fees here!</b> You only have to pay the Ethereum GAS. if you want to &#128591; us, you can donate to <a href="https://dfohub.com" target="_Blank">DFOhub</a> for more R&D and fancy updates</p>
             </section>
-    </section>
+        </section>
     );
     }
 });
