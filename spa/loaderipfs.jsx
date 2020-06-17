e823820b7e2a16d1048814e7a63444be1292ae84
alet89 <alessandrotoschi@me.com>
2020-06-17 23:15:17 +0200

    IPFS upload banner

--- spa/loaderipfs.jsx
+++ spa/loaderipfs.jsx
@@ -1,7 +1,27 @@
 var Loader = React.createClass({
+    
+    
+    
     render() { 
-        return (<div class="loadingio-spinner-ripple-6l4nh0xpwdg"><div class="ldio-2it1sjbil3o">
-        <div></div><div></div>
-        </div></div>);
+        return (
+        <section className="UPLOADBANNER">
+            <h1>Uploading...</h1>
+            <h1>{counter}</h1>
+            <h2>Waiting for Propagation</h2>
+            <div class="loadingio-spinner-ripple-6l4nh0xpwdg"><div class="ldio-2it1sjbil3o">
+            <div></div><div></div>
+            </div></div>
+        </section>
+        );
     }
 });
+
+function Counteripfs() {
+    const [counter, setCounter] = React.useState(60);
+  
+    React.useEffect(() => {
+      const timer =
+        counter > 0 && setInterval(() => setCounter(counter - 1), 1000);
+      return () => clearInterval(timer);
+    }, [counter]);
+    }
