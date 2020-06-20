9bebad00654490682bdcd21eb5f5970bb956bfd1
alet89 <alessandrotoschi@me.com>
2020-06-20 21:22:26 +0200

    ARTE vs ETRA

--- spa/deploy/nftOwner.jsx
+++ spa/deploy/nftOwner.jsx
@@ -9,12 +9,12 @@ var NftOwner = React.createClass({
         return (<section>
             <section className="DEPLOY-INFO">
                 <input id="dfo" name="type" type="radio" value="regular"/>
-                <label htmlFor="dfo">Co-Ownership:</label>
+                <label htmlFor="dfo">(ARTE)Co-Ownership:</label>
                 <p>You can mint this NFT in co-ownership with the Ethart Bank, receiving 100 ARTE and becoming an owner of the bank. <a href="https://github.com/b-u-i-d-l/ethArt">more</a></p>
             </section>
             <section className="DEPLOY-INFO">
                 <input id="standalone" name="type" type="radio" value="onchain"/>
-                <label htmlFor="standalone">Standalone:</label>
+                <label htmlFor="standalone">(ETRA)Standalone:</label>
                 <p>You have the full ownership of your NFT, and cheaper ETH fees to create it, but without any participation in the Ethart Bank.</p>
             </section>
         </section>);
